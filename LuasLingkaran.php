b217824bb98a2fb0ccbfccdcb1d6d46796d7a1a6
iindahrahma03-maker <iindah.rahma03@gmail.com>
2025-11-01 11:41:56 +0700

    pertemuan-4

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -4,10 +4,6 @@ class LuasLingkaran {
 
     public const phi = 3.14;
     public int $jari;
-    
-    public function __construct($isiJari = 1) {
-        $this->jari = $isiJari;
-    }
 
     public function tampil($nama = 'ban') {
         $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
@@ -18,15 +14,10 @@ class LuasLingkaran {
         echo "<br/>";
         echo "ini dari static";
     }
-
-    public function __destruct() {
-        echo "<br/>";
-        echo "udah ah cape";
-    }
 }
 
-$lingkaran = new LuasLingkaran(12);
-// $lingkaran->jari = 12;
+$lingkaran = new LuasLingkaran();
+$lingkaran->jari = 12;
 $lingkaran->tampil('roda'); //panggil method tampil
 
 LuasLingkaran::testing(); //panggil static method
